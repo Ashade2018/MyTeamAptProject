2b872554e7db3eab9316a21bf665e0b44bd40c2a
Eronmosele Victor <victoreronmosele@gmail.com>
2020-04-24 16:21:18 +0100

    Feature/project set up (Close #1) (#2)
    Set up Flutter project

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:league_app/src/ui/auth/splash_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: SplashScreen());
+  }
+}
