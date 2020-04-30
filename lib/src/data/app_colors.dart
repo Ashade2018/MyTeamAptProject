42434d5ca03c887fea97b4499b0be78fb77eefcd
Eronmosele Victor <victoreronmosele@gmail.com>
2020-04-30 10:15:15 +0100

    Feature/app colors (#13)
    * Add App Colors
    Co-authored-by: As sideeq <adebisisodiqadewale@gmail.com>

---
+++ lib/src/data/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = const Color(0xff177E89);
+  static const Color backgroundColor = const Color(0xff323031);
+}
