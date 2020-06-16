371c1288475ab7aa36deb3f4098757e73aa8af1e
Victor Eronmosele <victore@activedgetechnologies.com>
2020-06-16 11:38:35 +0100

    Add sponsors_screen.dart file

---
+++ lib/src/ui/pages/main/tabs/league/sponsors_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SponsorsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Sponsors Screen'),
+    );
+  }
+}
