4efcb5a5620dd63eb3818b690182ffd2d4b32f4a
Eronmosele Victor <victoreronmosele@gmail.com>
2020-07-16 14:10:27 +0100

    Update player model (#139)

--- lib/src/models/player.dart
+++ lib/src/models/player.dart
@@ -5,11 +5,16 @@ class Player {
   final String name;
   final DateTime dateOfBirth;
   final String nationality;
+  final String imageUrl;
+  final String publicImageId;
+
 
   Player({
     @required this.name,
     @required this.dateOfBirth,
     @required this.nationality,
+    @required this.imageUrl,
+    @required this.publicImageId,
   });
 
   factory Player.fromJson(String str) => Player.fromMap(json.decode(str));
@@ -20,12 +25,16 @@ class Player {
         name: json["name"],
         dateOfBirth: DateTime.parse(json["dateOfBirth"]),
         nationality: json["nationality"],
+        imageUrl: json["imageUrl"],
+        publicImageId: json["publicImageId"]
       );
 
   Map<String, dynamic> toMap() => {
         "name": name,
         "dateOfBirth":
             "${dateOfBirth.year.toString().padLeft(4, '0')}-${dateOfBirth.month.toString().padLeft(2, '0')}-${dateOfBirth.day.toString().padLeft(2, '0')}",
-        "nationality": nationality
+        "nationality": nationality,
+        "imageUrl": imageUrl,
+        "publicImageId": publicImageId
       };
 }
